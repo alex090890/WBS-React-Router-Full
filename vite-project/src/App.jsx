9aa62bd3b1e7936038459cd188c9820a2619d722
OLEKSANDR LAZURENKO <alexprofteach@gmail.com>
2024-02-15 10:34:36 +0000

    implemented all the files

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <nav>
+        <a className="link" href="#">
+          Students
+        </a>
+      </nav>
+      <div className="Instructions">
+        <div className="block">Render your components here</div>
+      </div>
+    </>
+  )
+}
+
+export default App
